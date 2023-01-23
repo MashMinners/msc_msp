2bda140dbcc77cbbec60a7ec2a294627d6193f91
Rain <Valehtelit@mail.ru>
2023-01-23 23:36:35 +1000

    # Восьмой пакет

--- src/Engine/Application.php
+++ src/Engine/Application.php
@@ -14,41 +14,82 @@ class Application
         require 'configs/routes.php';
     }
 
+    /**
+     * @param string $path
+     * @param $handler
+     * @return Route
+     */
     private function get(string $path, $handler) : Route
     {
         return $this->router->get( $path, $handler);
     }
 
+    /**
+     * @param string $path
+     * @param $handler
+     * @return Route
+     */
     private function post(string $path, $handler) : Route
     {
         return $this->router->post( $path, $handler);
     }
 
+    /**
+     * @param string $path
+     * @param $handler
+     * @return Route
+     */
     private function put(string $path, $handler) : Route
     {
         return $this->router->put( $path, $handler);
     }
 
+    /**
+     * @param string $path
+     * @param $handler
+     * @return Route
+     */
     private function delete(string $path, $handler) : Route
     {
         return $this->router->delete( $path, $handler);
     }
 
+    /**
+     * @param string $path
+     * @param $handler
+     * @return Route
+     */
     private function patch(string $path, $handler) : Route
     {
         return $this->router->patch( $path, $handler);
     }
 
+    /**
+     * @param string $path
+     * @param $handler
+     * @return Route
+     */
     private function head(string $path, $handler) : Route
     {
         return $this->router->head( $path, $handler);
     }
 
+    /**
+     * @param string $path
+     * @param $handler
+     * @return Route
+     */
     private function options(string $path, $handler) : Route
     {
         return $this->router->options( $path, $handler);
     }
 
+    /**
+     * @param ServerRequestInterface $request
+     * @return void
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function run(ServerRequestInterface $request){
         $response = $this->router->dispatch($request);
         $emitter = $this->container->get('Emitter');
